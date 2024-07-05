5072a2a01fe0773b10ace41e830bd99f9ee9afc3
danielkraus14 <danielk14@servimatsrl.com.ar>
2024-07-05 19:33:15 -0300

    feat(testimonials): testimonials component with initial styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "./_components/HeroSection.component";
 import AboutMe from "./_components/AboutMe/index";
 import Services from "./_components/Services";
 import CallToAction from "./_components/CallToAction";
+import Testimonials from "./_components/Testimonials";
 
 export default function Home() {
   return (
@@ -9,12 +10,13 @@ export default function Home() {
       className="flex min-h-screen flex-col py-5"
       style={{
         background:
-          "linear-gradient(0deg, rgba(130,171,165,1) 0%, rgba(255,255,255,1) 100%)",
+          "linear-gradient(0deg, rgba(130,171,165,1) 0% , rgba(240,255,245,1) 65% , rgba(255,255,255,1) 100%)",
       }}
     >
       <HeroSection />
       <AboutMe />
       <Services />
+      <Testimonials />
       <CallToAction />
     </main>
   );
