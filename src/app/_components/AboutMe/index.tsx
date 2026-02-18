9301d9337be1f33c1b1c48fb1a3e433fc7b62c31
Daniel <daniel.kraus@pinbus.com>
2026-02-17 21:06:28 -0300

    feat(ebook): changed text size at about me section

--- src/app/_components/AboutMe/index.tsx
+++ src/app/_components/AboutMe/index.tsx
@@ -30,26 +30,26 @@ const AboutMe: React.FC = () => {
           <h2 className=" pb-4 text-center text-3xl  font-semibold text-white md:text-justify">
             Sobre mí...
           </h2>
-          <p className="text-md text-justify text-white">
+          <p className="text-md text-justify text-white lg:text-lg">
             Soy Sergio Lépori, Licenciado en Psicología graduado en 2015.
           </p>
-          <p className="text-md text-justify text-white">
+          <p className="text-md text-justify text-white lg:text-lg">
             Desde entonces trabajo con personas que buscan equilibrar su salud
             mental y que esto influya de manera positiva en su vida.
           </p>
-          <p className="text-md text-justify text-white">
+          <p className="text-md text-justify text-white lg:text-lg">
             En cada sesión intentamos enfocarnos en el presente, evaluando las
             emociones y situaciones actuales, así como las experiencias pasadas
             que puedan influir en la actualidad.
           </p>
-          <p className="text-md text-justify text-white">
+          <p className="text-md text-justify text-white lg:text-lg">
             Para ello es necesario trabajar sobre el modo en que nos
             relacionamos con nosotros mismos y con los demás, promoviendo la
             aceptación y la comprensión del desarrollo que tuvimos a largo de
             nuestra vida, reevaluando y modificando ciertos aspectos que generen
             malestar.
           </p>
-          <p className="text-md text-justify text-white">
+          <p className="text-md text-justify text-white lg:text-lg">
             El fin de todo esto es encontrar bienestar en el Aquí y Ahora,
             fomentar un crecimiento continuo y funcional en la vida diaria.
           </p>
